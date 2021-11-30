05260198efc5d4b05f5ef1c1ce530bc3e5d9ec10
Borja Ferrer <borjetaa27@gmail.com>
2021-12-01 00:33:47 +0100

    primer commit

---
+++ BigBaseV2/src/gui/base_tab.cpp
@@ -0,0 +1,101 @@
+#include "common.hpp"
+#include "base_tab.h"
+#include "imgui.h"
+#include "script.hpp"
+#include "fiber_pool.hpp"
+#include "natives.hpp"
+#include "gta_util.hpp"
+#include "ImGuiBitfield.h"
+
+namespace big
+{
+	void base_tab::render_base_tab()
+	{
+		if (ImGui::BeginTabItem("Jugador"))
+		{
+			
+			
+
+
+			if (ImGui::Button("Godmode")) {
+				ENTITY::SET_ENTITY_INVINCIBLE(PLAYER::PLAYER_PED_ID(), true);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Desactivar god mode")) {
+				ENTITY::SET_ENTITY_INVINCIBLE(PLAYER::PLAYER_PED_ID(), false);
+			}
+			ImGui::Separator();
+			if (ImGui::Button("Invisible")) {
+				ENTITY::SET_ENTITY_VISIBLE(PLAYER::PLAYER_PED_ID(), false, 0);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Quitar Invisible")) {
+				ENTITY::SET_ENTITY_VISIBLE(PLAYER::PLAYER_PED_ID(), true, 0);
+			}
+			ImGui::Separator();
+			if (ImGui::Button("5 Estrellas fake")) {
+				MISC::SET_FAKE_WANTED_LEVEL(5);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Quitar policia fake")) {
+				MISC::SET_FAKE_WANTED_LEVEL(0);
+			}
+
+			if (ImGui::Button("Policia real")) {
+				PLAYER::SET_PLAYER_WANTED_LEVEL(PLAYER::PLAYER_ID(), 4, false);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Quitar policia")) {
+				PLAYER::SET_PLAYER_WANTED_LEVEL(PLAYER::PLAYER_ID(), 0, false);
+			}
+
+			ImGui::Separator();
+
+			if (ImGui::Button("Unload"))
+			{
+				g_running = false;
+			}
+			ImGui::EndTabItem();
+		}
+		if (ImGui::BeginTabItem("Vehiculo")) {
+			if (ImGui::Button("Spawn T20"))
+			{
+				QUEUE_JOB_BEGIN_CLAUSE()
+				{
+					constexpr auto hash = RAGE_JOAAT("T20");
+					while (!STREAMING::HAS_MODEL_LOADED(hash))
+					{
+						STREAMING::REQUEST_MODEL(hash);
+						script::get_current()->yield();
+					}
+
+					auto pos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
+					*(unsigned short*)g_pointers->m_model_spawn_bypass = 0x9090;
+					Vehicle vehicle = VEHICLE::CREATE_VEHICLE(hash, pos.x, pos.y, pos.z, 0.f, TRUE, FALSE, FALSE);
+					*(unsigned short*)g_pointers->m_model_spawn_bypass = 0x0574; //By writing the "old" bypass to the function, running CREATE_VEHICLE, then restoring it, the anti-cheat does not have enough time to catch the function in a dirty state.
+
+					script::get_current()->yield();
+					STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
+					if (*g_pointers->m_is_session_started)
+					{
+						DECORATOR::DECOR_SET_INT(vehicle, "MPBitset", 0);
+						ENTITY::_SET_ENTITY_SOMETHING(vehicle, TRUE); //True means it can be deleted by the engine when switching lobbies/missions/etc, false means the script is expected to clean it up.
+						auto networkId = NETWORK::VEH_TO_NET(vehicle);
+						if (NETWORK::NETWORK_GET_ENTITY_IS_NETWORKED(vehicle))
+							NETWORK::SET_NETWORK_ID_EXISTS_ON_ALL_MACHINES(networkId, true);
+						VEHICLE::SET_VEHICLE_IS_STOLEN(vehicle, FALSE);
+					}
+				}
+				QUEUE_JOB_END_CLAUSE
+			}
+
+			if (ImGui::Button("Cambiar la matricula del coche")) {
+				Ped playerPed = PLAYER::PLAYER_PED_ID();
+				Vehicle veh = PED::GET_VEHICLE_PED_IS_USING(playerPed);
+				char* plateText = "BORJA";
+				VEHICLE::SET_VEHICLE_NUMBER_PLATE_TEXT(veh, plateText);
+			}
+			ImGui::EndTabItem();
+		}
+	}
+}
